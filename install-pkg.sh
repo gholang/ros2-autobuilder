b4cab1a1710698e0b362bf86802d604aa049c7c2
yezune.choi <yezune.choi@lge.com>
2021-11-03 20:21:37 +0900

    remove unused file

--- install-pkg.sh
+++
@@ -1,5 +0,0 @@
-apt update && \
-apt install git build-essential python3-pip virtualenv enchant npm libcppunit-subunit-dev dumb-init && \
-pip3 install buildbot buildbot-www buildbot-waterfall-view buildbot-console-view buildbot-grid-view buildbot-worker testtools
-
-exit 0
