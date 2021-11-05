504983263300370e0734b79c0a9c67cbce60da3c
yezune.choi <yezune.choi@lge.com>
2021-11-05 11:21:07 +0900

    init dir

---
+++ init-dir.sh
@@ -0,0 +1,6 @@
+# To initialize directories for buildbot master and worker.
+mkdir -p buildbot/git/mirror
+mkdir -p buildbot/git/trash
+mkdir -p srv/autobuilder/autobuilder.yoctoproject.org
+mkdir -p srv/autobuilder/downloads.yoctoproject.org
+
